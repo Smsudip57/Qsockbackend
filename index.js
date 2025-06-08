d4e82a1e4a4926fe7e0531ec73cef7006b996186
sudip-codencoit <sudip.mandal@codencoit.com>
2025-06-09 03:01:24 +0600

    Update: fixed loading issue for www subdomain

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const dbConnect = require('./dbConnect/dbConnect'); // Import the database connection
-const auth = require('./routes/auth');
+const express = require("express");
+const dbConnect = require("./dbConnect/dbConnect"); // Import the database connection
+const auth = require("./routes/auth");
 // const chat = require('./routes/chatSession');
-const payment = require('./routes/payment');
-const user = require('./routes/user');
-const admin = require('./routes/admin');
-const { adminAuth, userAuth } = require('./middlewares/Auth');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const payment = require("./routes/payment");
+const user = require("./routes/user");
+const admin = require("./routes/admin");
+const { adminAuth, userAuth } = require("./middlewares/Auth");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-const http = require('http'); // Required to integrate Socket.IO
-const { Server } = require('socket.io'); // Import the Socket.IO server
-const setupSocket = require('./socket/socket');
-
+const http = require("http"); // Required to integrate Socket.IO
+const { Server } = require("socket.io"); // Import the Socket.IO server
+const setupSocket = require("./socket/socket");
 
 const server = http.createServer(app); // Create the HTTP server for Express and Socket.IO
 const io = setupSocket(server);
 
-
-app.options('*', cors({
-  origin: ['http://localhost:3000', 'https://qsocks.net', process.env.Client_Url],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  maxAge: 86400 // Cache preflight results for 24 hours
-}));
-
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://qsocks.net', process.env.Client_Url],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  maxAge: 86400 // Cache preflight results for 24 hours
-}))
-app.use(express.static('public'));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://qsocks.net",
+      "https://www.qsocks.net",
+      process.env.Client_Url,
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    maxAge: 86400,
+  })
+);
+app.use(express.static("public"));
 app.use(cookieParser());
-
-
-
 
 app.use((req, res, next) => {
   console.log(`Path hit: ${req.originalUrl}`);
   next();
 });
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use(express.json());
 
-
-
-app.use('/api', auth);
+app.use("/api", auth);
 // app.use('/api/chat', chat);
-app.use('/api/user', userAuth, user);
-app.use('/api/admin', adminAuth, admin);
-app.use('/api', payment);
-
-
+app.use("/api/user", userAuth, user);
+app.use("/api/admin", adminAuth, admin);
+app.use("/api", payment);
 
 app.use((req, res, next) => {
   console.log(`Path hit: ${req.originalUrl}`);
-  console.log('This route does not exist!');
-  res.status(404).send('This route does not exist!');
+  console.log("This route does not exist!");
+  res.status(404).send("This route does not exist!");
 });
 
 const start = async () => {
@@ -75,4 +66,4 @@ const start = async () => {
   });
 };
 
-start()
+start();
